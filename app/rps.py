7863fdf2994bd88f5962ab566aa93d6543fe0407
mr1900-source <mr1900@georgetown.edu>
2025-11-03 11:37:53 -0500

    Winner Determination

--- app/rps.py
+++ app/rps.py
@@ -1,6 +1,5 @@
 import random
 
-
 VALID_OPTIONS = ["rock", "paper", "scissors"]
 
 # ASK USER FOR AN INPUT (R/P/S)
@@ -22,4 +21,26 @@ print("COMP:", computer_choice)
 
 # DETERMINE THE WINNER
 
-print("TODO:", "DETERMINE WINNER")
+# quick alias to facilitate some copy and pasting
+# we will soon move this into a function anyway
+u = user_choice
+c = computer_choice
+
+if u == "rock" and c == "rock":
+    print("TIE GAME")
+elif u == "rock" and c == "paper":
+    print("COMPUTER WINS")
+elif u == "rock" and c == "scissors":
+    print("USER WINS")
+elif u == "paper" and c == "rock":
+    print("COMPUTER WINS") # OOPS
+elif u == "paper" and c == "paper":
+    print("TIE GAME")
+elif u == "paper" and c == "scissors":
+    print("USER WINS") # OOPS
+elif u == "scissors" and c == "rock":
+    print("COMPUTER WINS")
+elif u == "scissors" and c == "paper":
+    print("USER WINS")
+elif u == "scissors" and c == "scissors":
+    print("TIE GAME")
