aa248562d992bb12d18284ac20dac6f8990db3bb
mr1900-source <mr1900@georgetown.edu>
2025-11-05 11:38:28 -0500

    Testing (#4)
    * Refactor Winner Determination log
    
    * Testing

--- README.md
+++ README.md
@@ -1,35 +1,46 @@
-# my-first-repo
+cd ~/Desktop/my-first-repo-fall-2025
+```
 
-This is the README.md file. It uses the markdown language.
-
-Here is a lidt:
-    Item 1
-    Item 2
-    Item 3
-
-## Setup
-
-Clone the repo to donwoload it from GitHub.
-
-Navigate to the repo using the command line.
+Create a virtual environment:
 
 ```sh
-cd ~/Desktop/my-first-repo-fall-2025
+conda create -n my-first-env-fall-2025 python=3.11
+```
+
+Activate the virtual environment:
+
+```sh
+conda activate my-first-env-fall-2025
+```
+
+Install package dependencies:
+
+```sh
+pip install -r requirements.txt
 ```
 
 ## Usage
 
-Example Script:
-
+Example script:
+Example script:
 ```sh
 python app/my_script.py
 ```
-
-Game of rock.paper, scissors:
-
+Game of rock, paper, scissors:
 ```sh
 python app/rps.py
-
-# alternative modular style command:
+# alternative "modular style" command:
 python -m app.rps
+```
+
+# alternative "modular style" command:
+python -m app.rps
+```
+
+## Testing
+
+Run tests:
+
+```sh
+pytest
 ```
--- app/rps.py
+++ app/rps.py
@@ -21,26 +21,53 @@ print("COMP:", computer_choice)
 
 # DETERMINE THE WINNER
 
-# quick alias to facilitate some copy and pasting
-# we will soon move this into a function anyway
-u = user_choice
-c = computer_choice
+def determine_winner(u, c):
+    if u == "rock" and c == "rock":
+        return "TIE GAME"
+    elif u == "rock" and c == "paper":
+        return "COMPUTER WINS"
+    elif u == "rock" and c == "scissors":
+        return "USER WINS"
+    elif u == "paper" and c == "rock":
+        return "COMPUTER WINS" # OOPS
+    elif u == "paper" and c == "paper":
+        return "TIE GAME"
+    elif u == "paper" and c == "scissors":
+        return "USER WINS" # OOPS
+    elif u == "scissors" and c == "rock":
+        return "COMPUTER WINS"
+    elif u == "scissors" and c == "paper":
+        return "USER WINS"
+    elif u == "scissors" and c == "scissors":
+        return "TIE GAME"
 
-if u == "rock" and c == "rock":
-    print("TIE GAME")
-elif u == "rock" and c == "paper":
-    print("COMPUTER WINS")
-elif u == "rock" and c == "scissors":
-    print("USER WINS")
-elif u == "paper" and c == "rock":
-    print("COMPUTER WINS") # OOPS
-elif u == "paper" and c == "paper":
-    print("TIE GAME")
-elif u == "paper" and c == "scissors":
-    print("USER WINS") # OOPS
-elif u == "scissors" and c == "rock":
-    print("COMPUTER WINS")
-elif u == "scissors" and c == "paper":
-    print("USER WINS")
-elif u == "scissors" and c == "scissors":
-    print("TIE GAME")
+result = determine_winner(user_choice, computer_choice)
+print(result)
+
+# assert determine_winner("rock", "rock") == "TIE GAME"
+if __name__ == "__main__":
+    # ONLY RUN THE CODE BELOW
+    # IF WE ARE RUNNING THIS SCRIPT FROM THE COMMAND LINE
+    # BUT NOT IF WE'RE TRYING TO JUST IMPORT SOME STUFF FROM THIS FILE
+
+    # ASK USER FOR AN INPUT (R/P/S)
+
+    user_choice = input("Please choose one of 'rock', 'paper', or 'scissors': ")
+    print("USER:", user_choice)
+
+    # VALIDATIONS
+
+    if user_choice not in VALID_OPTIONS:
+        print("OOPS INVALID INPUT, PLEASE TRY AGAIN")
+        # exit()
+        quit()
+
+    # GENERATE RANDOM COMPUTER CHOICE
+
+    computer_choice = random.choice(VALID_OPTIONS)
+    print("COMP:", computer_choice)
+
+    # DETERMINE THE WINNER
+
+    result = determine_winner(user_choice, computer_choice)
+    print(result)
